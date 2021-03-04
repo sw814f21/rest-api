86b7f03559844cd0ebb85e9ad1b95f71b370006f
Seraph <31919330+Jakobzs@users.noreply.github.com>
2021-03-04 17:57:45 +0100

    Added REST API example with Tide

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,24 @@
-fn main() {
-    println!("Hello, world!");
+use async_std::println;
+use tide::Request;
+use tide::prelude::*;
+
+#[derive(Debug, Deserialize)]
+struct Animal {
+    name: String,
+    legs: u8,
+}
+
+#[async_std::main]
+async fn main() -> tide::Result<()> {
+    println!("Starting up web API").await;
+    let mut app = tide::new();
+    app.at("/orders/shoes").post(order_shoes);
+    println!("Listening for requests...").await;
+    app.listen("127.0.0.1:8080").await?;
+    Ok(())
+}
+
+async fn order_shoes(mut req: Request<()>) -> tide::Result {
+    let Animal { name, legs } = req.body_json().await?;
+    Ok(format!("Hello, {}! I've put in an order for {} shoes", name, legs).into())
 }
