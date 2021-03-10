7788d4bab1abb22fa3c88e29be57e64989ef9220
Seraph <31919330+Jakobzs@users.noreply.github.com>
2021-03-10 14:37:09 +0100

    added loading of database posts

--- src/main.rs
+++ src/main.rs
@@ -28,12 +28,31 @@ async fn manual_hello() -> impl Responder {
     HttpResponse::Ok().body("Hey there!")
 }
 
+
+use self::models::*;
+use self::diesel::prelude::*;
+
 #[actix_web::main]
 async fn main() -> std::io::Result<()> {
     dotenv().ok();
     
     let database_url = dotenv::var("DatabaseFile").unwrap();
+
+    use crate::schema::posts::dsl::*;
+
     let db_conn = establish_connection(database_url);
+
+    let results = posts.filter(published.eq(true))
+        .limit(5)
+        .load::<Post>(&db_conn)
+        .expect("Error loading posts");
+
+    println!("Displaying {} posts", results.len());
+    for post in results {
+        println!("{}", post.title);
+        println!("----------\n");
+        println!("{}", post.body);
+    }
 
     HttpServer::new(|| {
         App::new()
