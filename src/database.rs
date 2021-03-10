4871168e242497a067c788ef7dbdc9fa6715892a
Seraph <31919330+Jakobzs@users.noreply.github.com>
2021-03-10 14:16:19 +0100

    work

--- src/database.rs
+++ src/database.rs
@@ -6,3 +6,14 @@ pub fn establish_connection(database_url: String) -> SqliteConnection {
     SqliteConnection::establish(&database_url)
         .unwrap_or_else(|_| panic!("Error connecting to {}", database_url))
 }
+
+pub fn create_post(conn: &SqliteConnection, title: &str, body: &str) -> usize {
+    use crate::schema::posts;
+
+    let new_post = NewPost { title, body };
+
+    diesel::insert_into(posts::table)
+        .values(&new_post)
+        .execute(conn)
+        .expect("Error saving new post")
+}
