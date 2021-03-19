13bc89520e05ae3a75c319c74302711903c1e0b0
nicklashugoy <nicklas.hug@gmail.com>
2021-03-19 11:45:35 +0100

    Load json as vector

--- src/utils/data_loader.rs
+++ src/utils/data_loader.rs
@@ -1,4 +1,3 @@
-use std::collections::HashMap;
 use std::fs::File;
 use std::io::BufReader;
 use crate::database::establish_connection;
@@ -8,13 +7,13 @@ use indicatif::ProgressBar;
 pub fn load_data(path: &String){
     let file = File::open(path).expect("Can't open file from path");
     let reader = BufReader::new(file);
-    let r: HashMap<String, models::NewRestaurant> = serde_json::from_reader(reader).expect("Can't parse json");
+    let restaurants_vector: Vec<models::NewRestaurant> = serde_json::from_reader(reader).expect("Can't parse json");
 
     let connection_pool = establish_connection();
     let connection = connection_pool.get().expect("Can't get connection");
 
-    let pb = ProgressBar::new(r.len() as u64);
-    for (_,restaurant) in r {
+    let pb = ProgressBar::new(restaurants_vector.len() as u64);
+    for restaurant in restaurants_vector {
         models::create_restaurant(&connection, &restaurant);
         pb.inc(1);
     }
