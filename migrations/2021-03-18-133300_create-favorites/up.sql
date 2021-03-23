054884ab987e0f2f11a302eabf1065ffea2e5d37
ZcortezZ <jasv16@student.aau.dk>
2021-03-23 13:44:28 +0100

    modified database values to not be nullable

--- migrations/2021-03-18-133300_create-favorites/up.sql
+++ migrations/2021-03-18-133300_create-favorites/up.sql
@@ -1,11 +1,11 @@
 CREATE TABLE users (
-  token_id TEXT,
+  token_id TEXT NOT NULL,
   notifications int,
   PRIMARY KEY (token_id)
 );
 
 CREATE TABLE favorites (
-  resturant_id int,
+  resturant_id INTEGER NOT NULL,
   token_id TEXT,
   FOREIGN KEY (token_id) REFERENCES users(token_id) ON DELETE CASCADE,
   PRIMARY KEY (resturant_id, token_id)
