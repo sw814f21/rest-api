6aaa421669a8129032a64161327dd7b1c2e98e90
Jonas Andersen <jonas@anrsn.me>
2021-04-19 12:52:15 +0200

    Alter sample data

--- sample_data/sample.sql
+++ sample_data/sample.sql
@@ -1,14 +1,15 @@
 INSERT INTO restaurant 
   (id, smiley_restaurant_id, name, address, zipcode, city, cvr, pnr, latitude, longitude) 
     VALUES 
-  (1, 1, 'Min restaurant', 'Torvet 2', '8000', 'Aarhus C', '12345678', '1022505250', 56.2678987, 10.8153082),
-  (2, 2, 'Hansens Is', 'Supervej 54',  '9000', 'Aalborg', '87654321', '1022505251', 57.0206236, 9.9391328),
-  (3, 3, 'Jensens Bøfhus', 'Storegade 23C',  '5000', 'Odense C', '00000000', '1022505252', 55.351556,10.3821213);
+  (1, 548708, 'SAMPLE Min restaurant', 'Torvet 2', '8000', 'Aarhus C', '20067535', '1019551292', 56.2678987, 10.8153082),
+  (2, 570643, 'SAMPLE Hansens Is', 'Supervej 54',  '9000', 'Aalborg', '20067535', '1019552094', 57.0206236, 9.9391328),
+  (3, 3, 'SAMPLE Jensens Bøfhus', 'Storegade 23C',  '5000', 'Odense C', '00000000', '1022505252', 55.351556,10.3821213);
 
 INSERT INTO smiley_report 
   (id, restaurant_id, smiley, report_id, date) 
     VALUES 
   (1, 1, 1, 'Virk1862541', '2021-02-18'),
+  (2, 1, 2, 'Virk1766163', '2020-06-09'),
   (2, 1, 2, 'Virk1766163', '2020-06-09'),
   (3, 3, 4, 'Virk1638155', '2019-03-06');
 
