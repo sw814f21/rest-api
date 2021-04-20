5c669ea13c9b0e939461f530d64741cb9055ee97
Seraph <31919330+Jakobzs@users.noreply.github.com>
2021-04-20 14:36:10 +0200

    Attempt join

--- src/utils/data_loader.rs
+++ src/utils/data_loader.rs
@@ -1,7 +1,10 @@
-use crate::database::models::Version;
-use crate::utils::data_inserter::{insert_restaurant, insert_smileys};
 use crate::utils::json_parser::JsonRestaurant;
-use diesel::SqliteConnection;
+use crate::{database::models::Version, services::restaurant::restaurant};
+use crate::{
+    database::schema,
+    utils::data_inserter::{insert_restaurant, insert_smileys},
+};
+use diesel::{JoinOnDsl, QueryDsl, SqliteConnection};
 
 pub fn load_data_from_file(path: &String, conn: &SqliteConnection) {
     let json = std::fs::read_to_string(path).expect("Failed to read file");
@@ -24,5 +27,14 @@ pub fn insert_smiley_data(json: &String, connection: &SqliteConnection) {
 }
 
 pub fn get_data(conn: &SqliteConnection) {
+    let test = schema::smiley_report::table.inner_join(
+        schema::restaurant::table
+            .on(schema::restaurant::smiley_restaurant_id.eq(schema::smiley_report::restaurant_id)),
+    );
+
+    /*smiley_report::table
+    .filter(restaurant::version_number.gt(version))
+    .load::<Restaurant>(conn)
+    .expect("Failed to get restaurants based on version")*/
     // TODO: We should read the restaurant and smiley_report, and join on the restaurant_id. The result should be delivered as an output.
 }
